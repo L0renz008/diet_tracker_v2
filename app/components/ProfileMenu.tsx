c338b16b6b223a6961b270bcba3082e04e3c1e30
Lorenzo <lrenouvin98@gmail.com>
2024-07-26 12:50:07 +0200

    Add profile menu component

---
+++ app/components/ProfileMenu.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut, User } from "lucide-react";
+import Link from "next/link";
+
+const ProfileMenu = ({ user, expires }: Session) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" onClick={() => signOut()}>
+          Mon Compte
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <User className="mr-2 h-4 w-4" />
+          <Link href="/profile">Profile</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ProfileMenu;
