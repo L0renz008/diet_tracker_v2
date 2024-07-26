54b031118c62ef406cd58b6bd2f58854cbc0f5f9
Lorenzo <lrenouvin98@gmail.com>
2024-07-26 12:50:41 +0200

    Add layout for login page

---
+++ app/login/layout.tsx
@@ -0,0 +1,3 @@
+export default function LoginLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return <div className="h-screen w-full  flex justify-center items-center">{children}</div>;
+}
