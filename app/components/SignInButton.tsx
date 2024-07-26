d63e2d5713ac86d66b1a6658d1c345b85e63ac0b
Lorenzo <lrenouvin98@gmail.com>
2024-07-26 12:48:37 +0200

    Add signin and signout button

---
+++ app/components/SignInButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+
+export const SignInButton = () => {
+  return (
+    <Button variant={"outline"} onClick={() => signIn("", { callbackUrl: "/" })}>
+      Se connecter
+    </Button>
+  );
+};
